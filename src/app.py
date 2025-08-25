117a68f998c91f2140e219af81590cabaa8b1dfa
Alisson <alisontattooskate@gmail.com>
2025-08-25 20:30:15 -0300

    add

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ from utils.calculator import add, subtract, multiply, divide
 def main():
     st.title("Calculadora Simples")
     
-    # Entrada do usuário para os números
+    # teste teste teste
     num1 = st.number_input("Digite o primeiro número", value=0.0)
     num2 = st.number_input("Digite o segundo número", value=0.0)
     
